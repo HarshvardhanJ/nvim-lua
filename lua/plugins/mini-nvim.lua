2aa32e15ff3aeb5f3afc3e9da2fa4ce5a2c0e996
HarshvardhanJ <harshvardhanjp@gmail.com>
2025-07-05 01:54:27 +0530

    Update mini-nvim.lua

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -9,7 +9,19 @@ return {
     require("mini.comment").setup()      -- Comment toggling
     require("mini.surround").setup()     -- Surround text objects
     require("mini.move").setup()
-    require("mini.ai").setup()
+    require("mini.git").setup()
+    require("mini.ai").setup({
+      custom_textobjects = {
+        -- "o" for whole buffer
+        o = function()
+          local from = { line = 1, col = 1 }
+          local last_line = vim.fn.line("$")
+          local last_col = math.max(vim.fn.getline(last_line):len(), 1)
+          local to = { line = last_line, col = last_col }
+          return { from = from, to = to }
+        end,
+      },
+    })
 
   end,
 }
